cae95a668913d4a24f8c69726e5d3530cb6efb5f
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-05-10 13:30:53 +0530

    Add files via upload

--- DSA.java
+++ DSA.java
@@ -38,11 +38,51 @@
 
 //                                                   (day2 Reverse an array in place)
 
+// public class DSA{
+//     public static void main(String[] args) {
+//         int[] arr={1,2,3,4,5};
+//         for(int i=arr.length-1; i>=0; i--){
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+//                                    (day3 Find the 'Kth' max and min element of an array)
+// public class DSA{
+//     public static void main(String[] args) {
+//         int[] arr={12,3,5,7,19,0,23};
+//          int k=5;
+
+//         // Arrays.sort(arr);
+
+//         if(k <= arr.length){
+//             int kthMin = arr[k-1];
+//             int kthMax = arr[arr.length-k];
+//             System.out.println(k + "th minimum element"+ kthMin);
+//             System.out.println(k + " th is maximum element" + kthMax);
+
+//         }
+//         else{
+//             System.out.println("k is largest than the size of the array");
+//         }
+//     }
+// }
+
+//                                         (day4 /5. Move all negative numbers to beginning and positive to end)
 public class DSA{
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
-        for(int i=arr.length-1; i>=0; i--){
-            System.out.println(arr[i]);
+        int[] arr ={2,-4,5,-6,7,-9};
+        int j=0;
+        int i;
+        for(i=0; i<5; i++){
+            if ( arr[i]<0){
+                int temp= arr[i];
+                arr[i]= arr[j];
+                arr[j]= temp;
+                j++;
+                
+            }
         }
+        System.out.println(arr[j] );
     }
 }
