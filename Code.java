1fb8eed6656abd133c3ff11af76cebd07559e149
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-10-14 22:49:23 +0530

    Add files via upload

---
+++ Code.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class Code{
+    public static void main(String[] args){
+//         // System.out.print("maa");
+//         // String name  = " mata ";
+//         // int  a = 34; 
+//         // int  b = 45;
+//         //  System.out.println(name );
+//         Scanner sc= new Scanner( System.in);
+//         String name =sc.next();
+//         // nextInt();
+//         // nextFloat();
+//         System.out.println(name);
+
+//                              addition of two number 
+
+
+
+Scanner sc= new Scanner(System.in);
+int a= sc.nextInt();
+int b= sc.nextInt();
+int sum = a+b;
+System.out.println(sum );
+    }
+}
