3997478b6fe9a02531c8a9d72bffb1bc4f0198a0
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-11-01 00:08:25 +0530

    Add files via upload

--- Code.java
+++ Code.java
@@ -97,8 +97,37 @@ public class Code{
 //     i++;
 // }while(i<=20);
 //                               pattern
-for(int i=1; i<=4; i++){
-    System.out.println("*");
+// for(int i=1; i<=4; i++){
+//     for(int j=1; j<=4;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println("*");
+//     }
+// }
+
+//                       Hollow Ractangle pattern 
+    
+    // for(int i=1; i<=4; i++){
+    //     for(int j=1; j<=5; j++){
+    //         if(i==1 || j==1 || i==4 || j==5){
+    //             System.out.print("*");
+    //         }
+    //          else{
+    //         System.out.print(" ");
+    //     }
+    // }
+    //         System.out.println( );
+        
+        
+    //     }
+//                                Half pyramid pattern
+
+for( int i=1; i<=4; i++){
+    for(int j=1; j<=i; j++){
+        System.out.print("*");
+    }
+    System.out.println( );
 }
     }
-}
+    
+    }
