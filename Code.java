aaf3e7fd8657152defa2a3510bb8ba273b0a39ad
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-11-05 23:57:14 +0530

    Add files via upload

--- Code.java
+++ Code.java
@@ -203,19 +203,31 @@ public class Code{
 //     }
 
 //                      factorial number
- public static void printFactorial(int n){
-    int factorial=1;
-    for(int i=n; i>=1;i--){
-        factorial = factorial*i; 
-    }
-    System.out.println(factorial);
-    return ;
+//  public static void printFactorial(int n){
+//     int factorial=1;
+//     for(int i=n; i>=1;i--){
+//         factorial = factorial*i; 
+//     }
+//     System.out.println(factorial);
+//     return ;
 
 
- } 
+//  } 
+// public static void main(String[] args) {
+//     Scanner sc= new Scanner(System.in );
+//     int n= sc.nextInt();
+//     printFactorial(n);
+// }
+// }
+
+//                           array 
 public static void main(String[] args) {
-    Scanner sc= new Scanner(System.in );
-    int n= sc.nextInt();
-    printFactorial(n);
+    int[] marks = new int[3];
+    marks[0]= 97;
+    marks[1]=98;
+    marks[2]=95;
+     for(int i=0; i<3; i++){
+        System.out.println(marks[i]);
+     }
 }
 }
